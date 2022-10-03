e5095dd6dea673e21984c696c359004d9ab1ea24
Gakuto1112 <gakuto131112@gmail.com>
2022-10-03 23:33:57 +0900

    顔のパーツを制御するクラスの作成

---
+++ scripts/face_parts.lua
@@ -0,0 +1,86 @@
+---@class FacePartsClass 目と口を制御するクラス
+---@field EyeTypeID table EyeTypeとIDを紐付けるテーブル
+---@field MouthTypeID table MouthTypeとIDを紐付けるテーブル
+---@field HealthData table ダメージを受けたかどうか判定する為にHP情報を格納するテーブル
+---@field FacePartsClass.EmotionCount integer エモートの時間を計るカウンター
+---@field BlinkCount integer 瞬きのタイミングを計るカウンター
+
+FacePartsClass = {}
+
+EyeTypeID = {NONE = -1, NORMAL = 0, SURPLISED = 1, TIRED = 2, SLEEPY = 3, CLOSED = 4}
+MouthTypeID = {NONE = -1, CLOSED = 0, OPENED = 1}
+HealthData = {}
+EmotionCount = 0
+BlinkCount = 0
+
+---@alias EyeType
+---| "NONE"
+---| "NORMAL"
+---| "SURPLISED"
+---| "TIRED"
+---| "SLEEPY"
+---| "CLOSED"
+
+---@alias MouthType
+---| "NONE"
+---| "CLOSED"
+---| "OPENED"
+
+---表情を設定する。
+---@param rightEye EyeType 設定する右目の名前（"NONE"にすると変更されない）
+---@param leftEye EyeType 設定する左目の名前（"NONE"にすると変更されない）
+---@param mouth MouthType 設定する口の名前（"NONE"にすると変更されない）
+---@param duration integer この表情を有効にする時間
+---@param force boolean trueにすると以前のエモーションが再生中でも強制的に現在のエモーションを適用させる。
+function setEmotion(rightEye, leftEye, mouth, duration, force)
+	local rightEyePart = models.models.main.Head.FaceParts.Eyes.RightEye
+	local leftEyePart = models.models.main.Head.FaceParts.Eyes.LeftEye
+	local mouthPart = models.models.main.Head.FaceParts.Mouth
+	if EmotionCount == 0 or force then
+		--右目
+		if EyeTypeID[rightEye] >= 0 then
+			rightEyePart:setUVPixels(EyeTypeID[rightEye] * 6, 0)
+		end
+		--左目
+		if EyeTypeID[leftEye] >= 0 then
+			leftEyePart:setUVPixels(EyeTypeID[rightEye] * 6, 0)
+		end
+		--口
+		if MouthTypeID[mouth] >= 0 then
+			mouthPart:setUVPixels(MouthTypeID[mouth] * 2, 0)
+		end
+		EmotionCount = duration
+	end
+end
+
+events.TICK:register(function ()
+	local health = player:getHealth()
+	table.insert(HealthData, health)
+	if #HealthData == 3 then
+		table.remove(HealthData, 1)
+	end
+	if health < HealthData[1] then
+		if health == 0 then
+			setEmotion("SURPLISED", "SURPLISED", "CLOSED", 20, true)
+		else
+			setEmotion("SURPLISED", "SURPLISED", "CLOSED", 8, true)
+		end
+	end
+	if EmotionCount == 0 then
+		local gamemode = player:getGamemode()
+		if (player:getHealth() / player:getMaxHealth() <= 0.2 or player:getFood() <= 6 or player:getFrozenTicks() == 140) and (gamemode == "SURVIVAL" or gamemode == "ADVENTURE") then
+			setEmotion("TIRED", "TIRED", "NONE", 0, false)
+		else
+			setEmotion("NORMAL", "NORMAL", "NONE", 0, false)
+		end
+	end
+	if BlinkCount == 200 then
+		setEmotion("CLOSED", "CLOSED", "NONE", 2, false)
+		BlinkCount = 0
+	elseif not client.isPaused() then
+		BlinkCount = BlinkCount + 1
+	end
+	EmotionCount = EmotionCount > 0 and not client:isPaused() and EmotionCount - 1 or EmotionCount
+end)
+
+return FacePartsClass
--- scripts/main.lua
+++ scripts/main.lua
@@ -1,4 +1,7 @@
 events.ENTITY_INIT:register(function ()
+	--クラスのインスタンス化
+	FacePartsClass = require("scripts/face_parts")
+
 	--初期化処理
 	vanilla_model.PLAYER:setVisible(false)
 end)
