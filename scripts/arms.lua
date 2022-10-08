1abcd2f5f979d624b59ad9baa9a19d9dcd73a798
Gakuto1112 <gakuto131112@gmail.com>
2022-10-08 17:54:45 +0900

    テクスチャの変更とそれ関するコードの修正

--- scripts/arms.lua
+++ scripts/arms.lua
@@ -6,7 +6,7 @@ events.TICK:register(function()
 	local rightArm = models.models.main.Avatar.Body.Arms.RightArm
 	local leftArm = models.models.main.Avatar.Body.Arms.LeftArm
 	local playerPose = player:getPose()
-	if playerPose == "CROUCHING" then
+	if playerPose == "CROUCHING" and not renderer:isFirstPerson() then
 		for _, armPart in ipairs({rightArm, leftArm}) do
 			armPart:setPos(0, 3, 0)
 			armPart:setRot(30, 0, 0)
