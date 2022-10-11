d5ce78ab5db64b5bda4cb00d25c50fe1d67153e6
Gakuto1112 <gakuto131112@gmail.com>
2022-10-11 22:24:24 +0900

    濡れていると狐火が消えるように変更

--- scripts/fox_fire.lua
+++ scripts/fox_fire.lua
@@ -1,37 +1,35 @@
 ---@class FoxFireClass 狐火を制御するクラス
----@field NightVisionData table 狐火の開始。終了を検出する為に暗視の情報が格納されているデータ
----@field EnableFoxFire boolean 狐火の表示が有効かどうか
+---@field IsFoxFireEnabled boolean 狐火を出すかどうか
+---@field FoxFireEnableData table 狐火の開始。終了を検出する為に狐火が有効かどうかの情報が格納されているデータ
 ---@field FoxFirePos Vecotr3 狐火の座標
 ---@field FoxFireSpeed number 狐火の移動速度
 
 FoxFireClass = {}
-NightVisionData = {}
-EnableFoxFire = false
+IsFoxFireEnabled = false
+FoxFireEnableData = {}
 FoxFirePos = vectors.vec(0, 0, 0)
 FoxFireSpeed = 5
 
 events.TICK:register(function ()
-	local nightVision = General.getStatusEffect("night_vision")
-	table.insert(NightVisionData, nightVision and true or false)
-	if #NightVisionData == 3 then
-		table.remove(NightVisionData, 1)
+	IsFoxFireEnabled = (General.getStatusEffect("night_vision") and true or false) and WetClass.WetCount == 0
+	table.insert(FoxFireEnableData, IsFoxFireEnabled)
+	if #FoxFireEnableData == 3 then
+		table.remove(FoxFireEnableData, 1)
 	end
-	if nightVision then
-		if not NightVisionData[1] then
+	if IsFoxFireEnabled then
+		if not FoxFireEnableData[1] then
 			sounds:playSound("minecraft:item.firecharge.use", player:getPos(), 1, 2)
 		end
-		EnableFoxFire = true
 		particles:addParticle("minecraft:soul_fire_flame", FoxFirePos:copy():add((math.random() - 0.5) * 0.25, (math.random() - 0.5) * 0.25, (math.random() - 0.5) * 0.25))
 	else
-		if NightVisionData[1] then
+		if FoxFireEnableData[1] then
 			sounds:playSound("minecraft:block.fire.extinguish", player:getPos(), 1, 2)
 		end
-		EnableFoxFire = false
 	end
 end)
 
 events.WORLD_RENDER:register(function ()
-	if player and EnableFoxFire then
+	if player and IsFoxFireEnabled then
 		local playerPos = player:getPos():add(0, 2.5, 0)
 		local vectorToPlayer = playerPos:copy():sub(FoxFirePos)
 		local FPS = client:getFPS()
