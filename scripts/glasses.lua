bd4c0db62f112c0d45935db0450c2bcf1391425b
Gakuto1112 <gakuto131112@gmail.com>
2022-10-12 15:59:05 +0900

    一部のGUIを開いている際に眼鏡をかけるように変更

---
+++ scripts/glasses.lua
@@ -0,0 +1,28 @@
+---@class GlassesClass 眼鏡を制御するクラス
+---@field GlassesWorePrev boolean 前チックに眼鏡をかけていたかどうか
+
+GlassesClass = {}
+GlassesWorePrev = false
+
+--ping関数
+function pings.setGlassVisible(visible)
+	models.models.main.Avatar.Head.Glasses:setVisible(visible)
+end
+
+events.TICK:register(function ()
+	if host:isHost() then
+		local guiClassName = host:getScreen()
+		local glassesWear = guiClassName and (guiClassName == "class_486" or guiClassName == "class_473" or guiClassName == "class_3934" or guiClassName == "class_3935") or false
+		if glassesWear and not GlassesWorePrev then
+			pings.setGlassVisible(true)
+		elseif not glassesWear and GlassesWorePrev then
+			pings.setGlassVisible(false)
+		end
+		GlassesWorePrev = guiClassName and glassesWear or false
+	end
+end)
+
+models.models.main.Avatar.Head.Glasses:setVisible(false)
+models.models.main.Avatar.Head.Glasses:setPrimaryRenderType("TRANSLUCENT")
+
+return GlassesClass
