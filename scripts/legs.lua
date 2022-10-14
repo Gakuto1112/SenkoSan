1ba39c3e3130f49fb28cc1b67bb9c5b5862eabcc
Gakuto1112 <gakuto131112@gmail.com>
2022-10-14 17:24:14 +0900

    丈の長いスカートを着ている際に脚がスカートからはみ出ないように修正

--- scripts/legs.lua
+++ scripts/legs.lua
@@ -1,20 +1,36 @@
 ---@class LegsClass 脚を制御するクラス
+---@field LegSwingMultiplayer table 衣装ごとの脚の振り角度の倍率
+---@field IsSneaking boolean スニークしているかどうか
 
 LegsClass = {}
+LegSwingMultiplayer = {DEFAULT = 0, NIGHTWEAR = 0, DISGUISE = 0, MAID_A = -0.67, MAID_B = -0.83}
+IsSneaking = false
 
 events.TICK:register(function ()
 	local rightLeg = models.models.main.Avatar.Body.BodyBottom.Legs.RightLeg
 	local leftLeg = models.models.main.Avatar.Body.BodyBottom.Legs.LeftLeg
-	if player:getPose() == "CROUCHING" then
+	IsSneaking = player:getPose() == "CROUCHING"
+	if IsSneaking then
 		for _, legPart in ipairs({rightLeg, leftLeg}) do
 			legPart:setPos(0, 4, -4)
-			legPart:setRot(30, 0, 0)
 		end
 	else
 		for _, legPart in ipairs({rightLeg, leftLeg}) do
 			legPart:setPos(0, 0, 0)
-			legPart:setRot(0, 0, 0)
 		end
+	end
+end)
+
+events.RENDER:register(function ()
+	local rightLeg = models.models.main.Avatar.Body.BodyBottom.Legs.RightLeg
+	local leftLeg = models.models.main.Avatar.Body.BodyBottom.Legs.LeftLeg
+	local playerPose = player:getPose()
+	if (playerPose == "STANDING" or playerPose == "CROUCHING" or playerPose == "SWIMMING" or playerPose == "FALL_FLYING") and not player:getVehicle() then
+		rightLeg:setRot(vanilla_model.RIGHT_LEG:getOriginRot():mul(LegSwingMultiplayer[CostumeClass.CurrentCostume], 0, 0):add(IsSneaking and 30 or 0, 0, 0))
+		leftLeg:setRot(vanilla_model.LEFT_LEG:getOriginRot():mul(LegSwingMultiplayer[CostumeClass.CurrentCostume], 0, 0):add(IsSneaking and 30 or 0, 0, 0))
+	else
+		rightLeg:setRot()
+		leftLeg:setRot()
 	end
 end)
 
