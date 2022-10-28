a5db8e3da7315e2360afed06de5cf5365766f9b0
Gakuto1112 <gakuto131112@gmail.com>
2022-10-29 00:05:12 +0900

    メイド服の時の脚の動きがガクガクだったのを修正

--- scripts/legs.lua
+++ scripts/legs.lua
@@ -9,11 +9,7 @@ events.TICK:register(function ()
 	local leftLeg = models.models.main.Avatar.Body.BodyBottom.Legs.LeftLeg
 	local rightArmorLeg = models.models.armor.Avatar.Body.BodyBottom.Legs.RightLeg
 	local leftArmorLeg = models.models.armor.Avatar.Body.BodyBottom.Legs.LeftLeg
-	local multiplayer = LegSwingMultiplayer[CostumeClass.CurrentCostume] and LegSwingMultiplayer[CostumeClass.CurrentCostume] or 0
-	local playerPose = player:getPose()
-	local rightLegRot = vanilla_model.RIGHT_LEG:getOriginRot().x * multiplayer + (playerPose == "CROUCHING" and 30 or 0)
-	local leftLegRot = vanilla_model.LEFT_LEG:getOriginRot().x * multiplayer + (playerPose == "CROUCHING" and 30 or 0)
-	if playerPose == "CROUCHING" then
+	if player:getPose() == "CROUCHING" then
 		for _, legPart in ipairs({rightLeg, leftLeg}) do
 			legPart:setPos(0, 4, -4)
 		end
@@ -32,7 +28,18 @@ events.TICK:register(function ()
 			end
 		end
 	end
-	if (playerPose == "STANDING" or playerPose == "CROUCHING" or playerPose == "SWIMMING" or playerPose == "FALL_FLYING") and not player:getVehicle() then
+end)
+
+events.RENDER:register(function ()
+	local rightLeg = models.models.main.Avatar.Body.BodyBottom.Legs.RightLeg
+	local leftLeg = models.models.main.Avatar.Body.BodyBottom.Legs.LeftLeg
+	local rightArmorLeg = models.models.armor.Avatar.Body.BodyBottom.Legs.RightLeg
+	local leftArmorLeg = models.models.armor.Avatar.Body.BodyBottom.Legs.LeftLeg
+	local multiplayer = LegSwingMultiplayer[CostumeClass.CurrentCostume] and LegSwingMultiplayer[CostumeClass.CurrentCostume] or 0
+	local rightLegRot = vanilla_model.RIGHT_LEG:getOriginRot().x * multiplayer + (General.IsSneaking and 30 or 0)
+	local leftLegRot = vanilla_model.LEFT_LEG:getOriginRot().x * multiplayer + (General.IsSneaking and 30 or 0)
+	local playerPose = player:getPose()
+	if (playerPose == "STANDING" or playerPose == "CROUCHING" or playerPose == "SWIMMING" or playerPose == "FALL_FLYING") and not player:getVehicle() and not ArmorClass.ArmorVisible[3] then
 		rightLeg:setRot(rightLegRot, 0, 0)
 		leftLeg:setRot(leftLegRot, 0, 0)
 		if ArmorClass.ArmorVisible[3] then
@@ -41,11 +48,11 @@ events.TICK:register(function ()
 		end
 	else
 		for _, modelPart in ipairs({rightLeg, leftLeg}) do
-			modelPart:setRot(0, 0, 0)
+			modelPart:setRot()
 		end
 		if ArmorClass.ArmorVisible[3] then
 			for _, modelPart in ipairs({rightArmorLeg, leftArmorLeg}) do
-				modelPart:setRot(0, 0, 0)
+				modelPart:setRot()
 			end
 		end
 	end
