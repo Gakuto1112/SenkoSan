effacda4129883dff6d4115f5ad9a627594258c2
Gakuto1112 <gakuto131112@gmail.com>
2023-01-09 16:39:28 +0900

    クリスマスの検出処理の改良

--- scripts/christmas.lua
+++ scripts/christmas.lua
@@ -30,20 +30,8 @@ events.TICK:register(function ()
 end)
 
 if host:isHost() and Costume.CurrentCostume == "SANTA" then
-	---西暦1年1月1日からの経過日数を返す。
-	---@param year integer
-	---@param month integer
-	---@param day integer
-	---@return integer daysElapsed 西暦1年1月1日からの経過日数
-	function getDaysElapsed(year, month, day)
-		return 365 * (year - 1) + math.floor(year / 4) - math.floor(year / 100) + math.floor(year / 400) + 59 + math.floor(306 * ((month <= 2 and month + 12 or month) + 1) / 10) - 122 + day
-	end
-
-	local now = client:getSystemTime() / 1000
-	local day = math.floor(now / 86400)
-	local year = 1970 + math.floor(day / 365.2425)
-	local daysElapsedFromNewYear = day - getDaysElapsed(year, 1, 1) + getDaysElapsed(1970, 1, 1)
-	if daysElapsedFromNewYear >= 357 and daysElapsedFromNewYear <= 359 then
+	local date = client:getDate()
+	if date.month == 12 and date.day >= 24 and date.day <= 26 then
 		print(Language.getTranslate("message__merry_christmas"))
 		Christmas.MelodyCount = 0
 	end
