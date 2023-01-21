b8f15a1184c670948b33e94b38c7fc6948accbae
Gakuto1112 <gakuto131112@gmail.com>
2023-01-21 14:41:03 +0900

    他の衣装の袖のテクスチャの作成

--- scripts/sleeve.lua
+++ scripts/sleeve.lua
@@ -1,4 +1,16 @@
 ---@class Sleeve 袖を操作するクラス
+
+events.TICK:register(function ()
+	local sleeveVisible = (Costume.CurrentCostume == "DEFAULT" or Costume.CurrentCostume == "NIGHTWEAR" or Costume.CurrentCostume == "MAID_B" or Costume.CurrentCostume == "PURIFICATION" or Costume.CurrentCostume == "YUKATA" or Costume.CurrentCostume == "KNIT" or Costume.CurrentCostume == "SANTA") and not Kotatsu.IsAnimationPlaying
+	for _, modelPart in ipairs({models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase, models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase}) do
+		modelPart:setVisible(sleeveVisible)
+	end
+	local sleeveRibbonVisible = Costume.CurrentCostume == "DEFAULT" or Costume.CurrentCostume == "KNIT" or Costume.CurrentCostume == "SANTA"
+	for _, modelPart in ipairs({models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase.RightSleeve.RightSleeveRibbon, models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase.LeftSleeve.LeftSleeveRibbon}) do
+		modelPart:setVisible(sleeveVisible and sleeveRibbonVisible)
+	end
+end)
+
 events.RENDER:register(function ()
 	if models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase:getRot().x + models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase:getAnimRot().x >= 0 then
 		models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase:setPivot(5.5, 19, 2)
