ac5ad64a50b69378c8f509263d341d9c41c0c11e
Gakuto1112 <gakuto131112@gmail.com>
2023-02-19 19:46:31 +0900

    仙狐さん専用の記述が混じってしまったので削除

--- scripts/costume.lua
+++ scripts/costume.lua
@@ -231,9 +231,6 @@ Costume = {
 			Costume.setCostumeTextureOffset(16)
 		elseif costume == "KIMONO" then
 			models.models.main.Avatar.Head.CKimonoH:setVisible(true)
-			for _, modelPart in ipairs({models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase.RightSleeve.RightSleeveRibbon, models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase.LeftSleeve.LeftSleeveRibbon}) do
-				modelPart:setVisible(false)
-			end
 			Costume.setCostumeTextureOffset(17)
 			Apron.disable()
 		end
