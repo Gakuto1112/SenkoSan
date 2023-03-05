66b39b7ce9ac8daee0499a99bd53538ebf9d6a98
Gakuto1112 <gakuto131112@gmail.com>
2023-03-05 16:48:45 +0900

    ほんの少しだけコード量減少

--- scripts/skull.lua
+++ scripts/skull.lua
@@ -4,27 +4,21 @@
 
 Skull = {
 	SkullList = {"default", "figure_a", "figure_b", "figure_c", "figure_d"},
-	CurrentSkull = 1,
-
-	---頭モデル設定の初期処理
-	skullInit = function ()
-		local loadedData = Config.loadConfig("skull", 1)
-		if loadedData <= #Skull.SkullList then
-			Skull.CurrentSkull = loadedData
-		else
-			Config.saveConfig("skull", 1)
-		end
-	end
+	CurrentSkull = 1
 }
 
-events.SKULL_RENDER:register(function (delta, block, item)
+events.SKULL_RENDER:register(function (_, block, item)
 	models.models.skull:setVisible((block == nil and item == nil) or Skull.CurrentSkull == 1)
-	models.models.skull_figure_a:setVisible((block ~= nil or item ~= nil) and Skull.CurrentSkull == 2)
-	models.models.skull_figure_b:setVisible((block ~= nil or item ~= nil) and Skull.CurrentSkull == 3)
-	models.models.skull_figure_c:setVisible((block ~= nil or item ~= nil) and Skull.CurrentSkull == 4)
-	models.models.skull_figure_d:setVisible((block ~= nil or item ~= nil) and Skull.CurrentSkull == 5)
+	for index, modelPart in ipairs({models.models.skull_figure_a, models.models.skull_figure_b, models.models.skull_figure_c, models.models.skull_figure_d}) do
+		modelPart:setVisible((block ~= nil or item ~= nil) and Skull.CurrentSkull == index + 1)
+	end
 end)
 
-Skull.skullInit()
+local loadedData = Config.loadConfig("skull", 1)
+if loadedData <= #Skull.SkullList then
+	Skull.CurrentSkull = loadedData
+else
+	Config.saveConfig("skull", 1)
+end
 
 return Skull
