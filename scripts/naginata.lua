bd4363b4a72fa6256ac089482101af1bc7964af8
Gakuto1112 <gakuto131112@gmail.com>
2023-04-11 01:18:04 +0900

    薙刀の構え

--- scripts/naginata.lua
+++ scripts/naginata.lua
@@ -1,5 +1,5 @@
 ---@class Naginata 薙刀のモデルを制御するクラス
----@class EnableAnimation
+---@class EnableAnimation boolean 薙刀の構えが有効かどうか
 
 Naginata = {
     EnableAnimation = true
@@ -7,13 +7,21 @@ Naginata = {
 
 events.TICK:register(function ()
     local leftHanded = player:isLeftHanded()
+    local active = player:getActiveItem().id ~= "minecraft:air"
     local firstPerson = renderer:isFirstPerson()
-    local rightSword = player:getHeldItem(leftHanded).id:find("^minecraft:.+_sword$") ~= nil and not firstPerson
-    local leftSword = player:getHeldItem(not leftHanded).id:find("^minecraft:.+_sword$") ~= nil and not firstPerson
-    vanilla_model.RIGHT_ITEM:setVisible(not (rightSword or Arms.ItemHeldContradicts))
-    vanilla_model.LEFT_ITEM:setVisible(not (leftSword or Arms.ItemHeldContradicts))
-    models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightNaginata:setVisible(rightSword and not Arms.ItemHeldContradicts)
-    models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftNaginata:setVisible(leftSword and not Arms.ItemHeldContradicts)
+    local rightSword = not (player:getHeldItem(leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts or (active and not leftHanded))
+    local leftSword = not (player:getHeldItem(not leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts or (active and leftHanded))
+    vanilla_model.RIGHT_ITEM:setVisible(not rightSword)
+    vanilla_model.LEFT_ITEM:setVisible(not leftSword)
+    models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightNaginata:setVisible(rightSword)
+    models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftNaginata:setVisible(leftSword)
+    for _, animation in ipairs({animations["models.main"]["naginata_right"], animations["models.naginata"]["naginata_right"]}) do
+        animation:setPlaying(rightSword and not leftHanded)
+    end
+    for _, animation in ipairs({animations["models.main"]["naginata_left"], animations["models.naginata"]["naginata_left"]}) do
+        animation:setPlaying(leftSword and leftHanded)
+    end
+    Naginata.EnableAnimation = (rightSword and not leftHanded) or (leftSword and leftHanded)
 end)
 
 return Naginata
