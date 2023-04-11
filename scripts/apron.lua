8f49424f470809a32f2080bbcc5f73d39c6745a7
Gakuto1112 <gakuto131112@gmail.com>
2023-04-11 23:46:34 +0900

    薙刀を持ってスニークする時の下半身のずれを修正

--- scripts/apron.lua
+++ scripts/apron.lua
@@ -11,8 +11,9 @@ Apron = {
 		events.RENDER:register(function ()
 			if not SitDown.IsAnimationPlaying then
 				if player:isCrouching() then
-					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setRot(vectors.vec3(math.max(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x) + 30) + Apron.RotOffset)
-					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setPos(0, 4.5, 1.5)
+					local legsYRot = math.rad(models.models.main.Avatar.Body.BodyBottom.Legs:getTrueRot().y)
+					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setRot(vectors.vec3(math.max(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x) + 30 * math.cos(legsYRot), 0, 30 * math.sin(legsYRot)) + Apron.RotOffset)
+					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setPos(-legsYRot * 1.2, 4.5, -math.abs(legsYRot) * 0.6 + 1.5)
 					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setPivot(0, 14.5, -2)
 				else
 					models.models.main.Avatar.Body.BodyBottom.Legs.Apron:setRot(vectors.vec3(math.max(vanilla_model.RIGHT_LEG:getOriginRot().x, vanilla_model.LEFT_LEG:getOriginRot().x)) + Apron.RotOffset)
