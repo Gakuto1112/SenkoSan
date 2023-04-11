6a3f690b3ec5997616b8bc2dd0f63a937b64ece7
Gakuto1112 <gakuto131112@gmail.com>
2023-04-11 22:17:13 +0900

    袖の動きの処理の変更

--- scripts/sleeve.lua
+++ scripts/sleeve.lua
@@ -15,43 +15,31 @@ Sleeve = {
 		end
 		events.RENDER:remove("sleeve_render")
 		events.RENDER:register(function ()
+			local armRot = {models.models.main.Avatar.Body.Arms.RightArm:getTrueRot() + (models.models.main.Avatar.Body.Arms.RightArm:getParentType() == "RightArm" and vanilla_model.RIGHT_ARM:getOriginRot() or vectors.vec3()), models.models.main.Avatar.Body.Arms.LeftArm:getTrueRot() + (models.models.main.Avatar.Body.Arms.LeftArm:getParentType() == "LeftArm" and vanilla_model.LEFT_ARM:getOriginRot() or vectors.vec3())}
 			local sleeveRot = {vectors.vec3(), vectors.vec3()}
-			local rotLimit = {{{-20, 20}, {-20, 20}}, {{-60, 60}, {-70, 70}}} --袖の可動範囲：1. 袖ベース：{1-1. 前後方向, 1-2. 左右方向}, 2. 袖：{2-1. 前後方向, 2-2. 左右方向}
 			if not renderer:isFirstPerson() and Sleeve.Moving then
 				local playerPose = player:getPose()
 				if playerPose == "FALL_FLYING" then
-					sleeveRot = {vectors.vec3(math.clamp(90 - vanilla_model.RIGHT_ARM:getOriginRot().x, rotLimit[1][1][1] + rotLimit[1][2][1], rotLimit[1][1][2] + rotLimit[1][1][2])), vectors.vec3(math.clamp(90 - vanilla_model.LEFT_ARM:getOriginRot().x, rotLimit[1][1][1] + rotLimit[2][1][1], rotLimit[1][1][2] + rotLimit[2][1][2]))}
+					sleeveRot = {vectors.vec3(math.clamp(90 - armRot[1].x, -80, 80)), vectors.vec3(math.clamp(90 - armRot[2].x, -80, 80))}
 				elseif playerPose == "SWIMMING" then
-					local rightOriginArmRot = vanilla_model.RIGHT_ARM:getOriginRot()
-					local leftOriginArmRot = vanilla_model.LEFT_ARM:getOriginRot()
-					sleeveRot = {vectors.vec3(math.clamp(rightOriginArmRot.x >= 90 and -rightOriginArmRot.x + 180 or -rightOriginArmRot.x, rotLimit[1][1][1] + rotLimit[2][1][1], rotLimit[1][1][2] + rotLimit[2][1][2])), vectors.vec3(math.clamp(leftOriginArmRot.x >= 90 and -leftOriginArmRot.x + 180 or -leftOriginArmRot.x, rotLimit[1][1][1] + rotLimit[2][1][1], rotLimit[1][1][2] + rotLimit[2][1][2]))}
+					sleeveRot = {vectors.vec3(math.clamp(armRot[1].x >= 90 and -armRot[1].x + 180 or -armRot[1].x, -80, 80)), vectors.vec3(math.clamp(armRot[2].x >= 90 and -armRot[2].x + 180 or -armRot[2].x, -80, 80))}
 				else
-					local leftHanded = player:isLeftHanded()
-					local vehicle = player:getVehicle()
-					sleeveRot = {vectors.vec3(math.clamp((Umbrella.IsUsing and leftHanded) and 70 or (90 - vanilla_model.RIGHT_ARM:getOriginRot().x), rotLimit[1][1][1] + rotLimit[2][1][1], rotLimit[1][1][2] + rotLimit[2][1][2]), vehicle and 20 or 0), vectors.vec3(math.clamp((Umbrella.IsUsing and not leftHanded) and 70 or (90 - vanilla_model.LEFT_ARM:getOriginRot().x), rotLimit[1][1][1] + rotLimit[2][1][1], rotLimit[1][1][2] + rotLimit[2][1][2]), vehicle and -20 or 0)}
+					local vehicle = player:getVehicle() ~= nil
+					sleeveRot = {vectors.vec3(math.clamp(90 - armRot[1].x, -80, 80), vehicle and 20 or 0), vectors.vec3(math.clamp(90 - armRot[2].x, -80, 80), vehicle and -20 or 0)}
 				end
 			end
-
-			local function sleeveBaseRotClamp(axis, index)
-				return index <= 2 and math.clamp(axis, rotLimit[1][index][1], rotLimit[1][index][2]) or axis
-			end
-
-			models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase:setRot(sleeveRot[1]:add(Sleeve.RightSleeveRotOffset):copy():applyFunc(function (axis, index)
-				return sleeveBaseRotClamp(axis, index)
+			models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase:setRot(sleeveRot[1]:add(Sleeve.RightSleeveRotOffset):copy():applyFunc(function (axis)
+				return math.clamp(axis, -20, 20)
 			end))
-			models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase:setRot(sleeveRot[2]:add(Sleeve.LeftSleeveRotOffset):copy():applyFunc(function (axis, index)
-				return sleeveBaseRotClamp(axis, index)
+			models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase:setRot(sleeveRot[2]:add(Sleeve.LeftSleeveRotOffset):copy():applyFunc(function (axis)
+				return math.clamp(axis, -20, 20)
 			end))
 			for index, sleeve in ipairs({models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightSleeveBase.RightSleeve, models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftSleeveBase.LeftSleeve}) do
-				sleeve:setRot(sleeveRot[index]:applyFunc(function (axis, vectorIndex)
-					if vectorIndex <= 2 then
-						if axis >= 0 then
-							return math.max(axis + rotLimit[1][vectorIndex][1], 0)
-						else
-							return math.min(axis + rotLimit[1][vectorIndex][2], 0)
-						end
+				sleeve:setRot(sleeveRot[index]:applyFunc(function (axis)
+					if axis >= 0 then
+						return math.max(axis - 20, 0)
 					else
-						return axis
+						return math.min(axis + 20, 0)
 					end
 				end))
 			end
