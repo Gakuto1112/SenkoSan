ca9f6f0323bf80bb94eee03c1aa815d8d5148f6e
Gakuto1112 <gakuto131112@gmail.com>
2023-04-12 00:01:20 +0900

    元のアイテムが表示される問題を修正

--- scripts/naginata.lua
+++ scripts/naginata.lua
@@ -7,21 +7,21 @@ Naginata = {
 
 events.TICK:register(function ()
     local leftHanded = player:isLeftHanded()
-    local active = player:getActiveItem().id ~= "minecraft:air"
     local firstPerson = renderer:isFirstPerson()
-    local rightSword = not (player:getHeldItem(leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts or (active and not leftHanded))
-    local leftSword = not (player:getHeldItem(not leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts or (active and leftHanded))
-    vanilla_model.RIGHT_ITEM:setVisible(not rightSword)
-    vanilla_model.LEFT_ITEM:setVisible(not leftSword)
+    local rightSword = not (player:getHeldItem(leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts)
+    local leftSword = not (player:getHeldItem(not leftHanded).id:find("^minecraft:.+_sword$") == nil or firstPerson or Arms.ItemHeldContradicts)
+    vanilla_model.RIGHT_ITEM:setVisible(not (rightSword or Arms.ItemHeldContradicts))
+    vanilla_model.LEFT_ITEM:setVisible(not (leftSword or Arms.ItemHeldContradicts))
     models.models.main.Avatar.Body.Arms.RightArm.RightArmBottom.RightNaginata:setVisible(rightSword)
     models.models.main.Avatar.Body.Arms.LeftArm.LeftArmBottom.LeftNaginata:setVisible(leftSword)
+    local active = player:getActiveItem().id ~= "minecraft:air"
     for _, animation in ipairs({animations["models.main"]["naginata_right"], animations["models.naginata"]["naginata_right"]}) do
-        animation:setPlaying(rightSword and not leftHanded)
+        animation:setPlaying(rightSword and not leftHanded and not active)
     end
     for _, animation in ipairs({animations["models.main"]["naginata_left"], animations["models.naginata"]["naginata_left"]}) do
-        animation:setPlaying(leftSword and leftHanded)
+        animation:setPlaying(leftSword and leftHanded and not active)
     end
-    Naginata.EnableAnimation = (rightSword and not leftHanded) or (leftSword and leftHanded)
+    Naginata.EnableAnimation = ((rightSword and not leftHanded) or (leftSword and leftHanded)) and not active
 end)
 
 return Naginata
