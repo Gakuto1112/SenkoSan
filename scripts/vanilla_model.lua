711e6607a53c459834c569fd0fe2a3dfb7183626
Gakuto1112 <gakuto131112@gmail.com>
2023-07-09 14:09:56 +0900

    パーツの親タイプの初期設定をavatar.jsonに移動

--- scripts/vanilla_model.lua
+++ scripts/vanilla_model.lua
@@ -3,6 +3,3 @@
 for _, vanillaModel in ipairs({vanilla_model.PLAYER, vanilla_model.ARMOR}) do
 	vanillaModel:setVisible(false)
 end
-for _, modelPart in ipairs({models.models.main.Avatar.Torso.Body.BodyBottom, models.models.main.Avatar.Torso.Body.BodyBottom.Legs.RightLeg.RightLegBottom, models.models.main.Avatar.Torso.Body.BodyBottom.Legs.LeftLeg.LeftLegBottom, models.models.main.Avatar.Torso.Arms.RightArm.RightArmBottom, models.models.main.Avatar.Torso.Arms.LeftArm.LeftArmBottom}) do
-	modelPart:setParentType("None")
-end
