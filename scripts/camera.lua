00b03b600fc3c7bc4eeefa9a06253cc3aa442d68
Gakuto1112 <gakuto131112@gmail.com>
2023-07-13 19:35:24 +0900

    カメラ処理をrenderに移動

--- scripts/camera.lua
+++ scripts/camera.lua
@@ -4,18 +4,31 @@ Camera = {
 	CameraOffset = 0
 }
 
+---このレンダーで処理を行ったかどうか
+---@type boolean
+local renderProcessed = false
+
+events.RENDER:register(function ()
+	if not renderProcessed then
+		local currentCameraOffset = renderer:getCameraOffsetPivot()
+		if currentCameraOffset == nil then
+			currentCameraOffset = 0
+		else
+			currentCameraOffset = currentCameraOffset.y
+		end
+		if currentCameraOffset ~= Camera.CameraOffset then
+			local cameraHeight = vectors.vec3(0, currentCameraOffset > Camera.CameraOffset and math.max(currentCameraOffset - 3 / client:getFPS(), Camera.CameraOffset) or math.min(currentCameraOffset + 3 / client:getFPS(), Camera.CameraOffset))
+			renderer:offsetCameraPivot(cameraHeight)
+			renderer:eyeOffset(cameraHeight)
+		end
+		renderProcessed = true
+	end
+end)
+
 events.WORLD_RENDER:register(function ()
-	local currentCameraOffset = renderer:getCameraOffsetPivot()
-	if currentCameraOffset == nil then
-		currentCameraOffset = 0
-	else
-		currentCameraOffset = currentCameraOffset.y
-	end
-	if currentCameraOffset ~= Camera.CameraOffset then
-		local cameraHeight = vectors.vec3(0, currentCameraOffset > Camera.CameraOffset and math.max(currentCameraOffset - 3 / client:getFPS(), Camera.CameraOffset) or math.min(currentCameraOffset + 3 / client:getFPS(), Camera.CameraOffset))
-		renderer:offsetCameraPivot(cameraHeight)
-		renderer:eyeOffset(cameraHeight)
-	end
+	if not client:isPaused() then
+        renderProcessed = false
+    end
 end)
 
 return Camera
