23bdfba5ecfb21164e2096f5537c412204d45156
Gakuto1112 <gakuto131112@gmail.com>
2023-10-01 13:29:11 +0900

    一人称視点で影/ペーパードールの袖が動くように変更

--- scripts/apron.lua
+++ scripts/apron.lua
@@ -6,11 +6,12 @@ Apron = {
 	---エプロンを有効にする。
 	enable = function ()
 		models.models.main.Avatar.LowerBody.Apron:setVisible(true)
-		events.RENDER:remove("apron_render")
-		events.RENDER:register(function ()
-			local leftLegRot = vanilla_model.LEFT_LEG:getOriginRot().x
-			models.models.main.Avatar.LowerBody.Apron:setRot(vectors.vec3(leftLegRot > 0 and leftLegRot * 2 or 0, 0.28647) + Apron.RotOffset)
-		end, "apron_render")
+		if events.RENDER:getRegisteredCount("apron_render") == 0 then
+			events.RENDER:register(function ()
+				local leftLegRot = vanilla_model.LEFT_LEG:getOriginRot().x
+				models.models.main.Avatar.LowerBody.Apron:setRot(vectors.vec3(leftLegRot > 0 and leftLegRot * 2 or 0, 0.28647) + Apron.RotOffset)
+			end, "apron_render")
+		end
 	end,
 
 	---エプロンを無効にする。
