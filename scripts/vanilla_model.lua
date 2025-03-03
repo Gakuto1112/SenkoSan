da882e8af7f92b8ba1090cc5c56ff4af50c2fdc7
Gakuto1112 <gakuto131112@gmail.com>
2025-03-03 23:02:35 +0900

    スクリプトをバニラ防具流用に対応

--- scripts/vanilla_model.lua
+++ scripts/vanilla_model.lua
@@ -1,5 +1,5 @@
 ---@class VanillaModel バニラーのモデルの処理クラス
 
-for _, vanillaModel in ipairs({vanilla_model.PLAYER, vanilla_model.ARMOR}) do
+for _, vanillaModel in ipairs({vanilla_model.PLAYER, vanilla_model.CHESTPLATE_RIGHT_ARM, vanilla_model.CHESTPLATE_LEFT_ARM, vanilla_model.LEGGINGS_RIGHT_LEG, vanilla_model.LEGGINGS_LEFT_LEG, vanilla_model.BOOTS}) do
 	vanillaModel:setVisible(false)
 end
