2c324e0c69094494edd4bb6cdf500a42d9a965e4
Gakuto1112 <gakuto131112@gmail.com>
2025-03-05 13:45:26 +0900

    メッセージの文体をキャラクターに合わせたものに変更

--- scripts/update_checker.lua
+++ scripts/update_checker.lua
@@ -68,10 +68,10 @@ UpdateChecker = {
             action_title_2 = "（左くりっく）";
             action_title_3 = "最新あばたーのばーじょんのりんくをこぴー";
             action_title_4 = "（右くりっく）";
-            msg_networking_api = "あっぷでーとの確認機能を有効にするにはFiguraの設定から、FiguraのNetworkingの使用を許可して、\"api.github.com\"を許可りすとに入れる必要があるのじゃ！";
-            msg_ongoing = "今あっぷでーとの確認中をしているところなのじゃ！お主もせっかちじゃのう～";
-            msg_copied = "最新のあばたーへのりんくをこぴーしたのじゃ。お主ぶらうざで開くのじゃ！";
-            msg_cannot_check_latest = "あばたーの最新ばーじょんを確認できぬから、りんくを取得できないのじゃ...。すまぬのう...。";
+            msg_networking_api = "あっぷでーとの確認機能を有効にするにはFiguraの設定から、FiguraのNetworkingの使用を許可して、\"api.github.com\"を許可りすとに入れる必要があります！";
+            msg_ongoing = "今あっぷでーとの確認中をしているところなんですよ！あなたもせっかちさんですね...！";
+            msg_copied = "最新のあばたーへのりんくをこぴーしました。あなたぶらうざで開いてください！";
+            msg_cannot_check_latest = "あばたーの最新ばーじょんを確認できないから、りんくを取得できませんでした...。ごめんなさい...。";
         };
     };
 
